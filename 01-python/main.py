b1ba9a2bcfa53e3b9a868973793c0568d0a682ed
Tomáš Ljutenko <485503@mail.muni.cz>
2021-12-01 16:54:50 +0100

    [Dat 1] **

---
+++ 01-python/main.py
@@ -0,0 +1,21 @@
+
+def calc_depth_increments(depths):
+    increments = 0
+    x, *xs = depths
+    for y in xs:
+        if x < y:
+            increments += 1
+        x = y
+    return increments
+
+def map_to_window(depths, size):
+    for i in range(len(depths) - size + 1):
+        yield sum(depths[i:i+size])
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        depths = [int(depth) for depth in file.read().split()]
+    
+        star1 = calc_depth_increments(depths)
+        star2 = calc_depth_increments(map_to_window(depths, 3))
+        print(star2)
