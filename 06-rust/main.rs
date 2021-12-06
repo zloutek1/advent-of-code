acc57fd390ac14f6d74b5a5522b09915503ace64
Tomáš Ljutenko <485503@mail.muni.cz>
2021-12-06 09:50:39 +0100

    [Day 06] **

--- 06-rust/main.rs
+++ 06-rust/main.rs
@@ -1,31 +1,31 @@
 use std::fs;
 use std::io;
 
-fn next_day(fishes: Vec<i64>) -> Vec<i64> {
-    let mut aged = fishes.iter()
-                         .map(|x| x - 1)
-                         .collect::<Vec<i64>>();
-    for i in 0..aged.len() {
-        if aged[i] < 0 {
-            aged[i] = 6;
-            aged.push(8);
-        }
-    }
-
-    return aged;
+fn next_day(mut fishes: Vec<usize>) -> Vec<usize> {
+    let parents: usize = fishes[0];
+    
+    fishes.rotate_left(1);
+    fishes[6] += parents;
+    
+    return fishes;
 }
 
-fn parse_file(path: &str) -> io::Result<Vec<i64>> {
+fn parse_file(path: &str) -> io::Result<Vec<usize>> {
     let contents = fs::read_to_string(path)?;
     let values = contents.trim()
                          .split(",")
-                         .map(|item| item.parse::<i64>().unwrap())
-                         .collect::<Vec<i64>>();
-    return Ok(values);
+                         .map(|item| item.parse::<usize>().unwrap())
+                         .collect::<Vec<usize>>();
+
+    let mut days = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for value in values.iter() {
+        days[*value] += 1;
+    }
+
+    return Ok(days);
 }
 
 fn main() {
-    println!("Hello, World!");
     let mut fishes = match parse_file("input.txt") {
         Ok(fishes) => fishes,
         Err(e) => panic!("[Error]: {}, failed parsing input", e)
@@ -34,5 +34,10 @@ fn main() {
     for _ in 0..80 {
         fishes = next_day(fishes);
     }
-    println!("{:?}", fishes.len());
+    println!("[Part 1] {}", fishes.iter().sum::<usize>());
+
+    for _ in 80..256 {
+        fishes = next_day(fishes);
+    }
+    println!("[Part 2] {}", fishes.iter().sum::<usize>());
 }
