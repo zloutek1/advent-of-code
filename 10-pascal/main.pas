0352f4f2ac7e89cd687e2acf598ea1515b2402c8
Tomáš Ljutenko <485503@mail.muni.cz>
2021-12-10 13:52:17 +0100

    [Day10] codestyle

--- 10-pascal/main.pas
+++ 10-pascal/main.pas
@@ -1,191 +1,189 @@
-program ReadFile;
 
-uses
-    SysUtils, Contnrs;
+Program ReadFile;
 
-type
-    TChar = class(TObject)
-        value : char;
-    end;
+Uses
+SysUtils, Contnrs;
 
-const
-    OPEN_BRACKETS: array[0..3] of char = ('(', '[', '{', '<');
-    CLOSE_BRACKETS: array[0..3] of char = (')', ']', '}', '>');
+Type
+  TChar = Class(TObject)
+    value : char;
+  End;
 
-function calcIllegalScore(line_content: string): integer;
-    const
-        SCORE_MAP: array[1..4] of integer = (3, 57, 1197, 25137);
+Const
+  OPEN_BRACKETS: array[0..3] Of char = ('(', '[', '{', '<');
+  CLOSE_BRACKETS: array[0..3] Of char = (')', ']', '}', '>');
 
-    var
-        c: char;
-        c_obj: TChar;
-        top: TChar;
-        seen_stack: TObjectStack;
+Function CalcIllegalScore(line_content: String): integer;
+  Const
+    SCORE_MAP: array[1..4] Of integer = (3, 57, 1197, 25137);
 
-    begin
-        seen_stack := TObjectStack.Create;
-        calcIllegalScore := 0;
-        for c in line_content do
-        begin
-            if (pos(c, OPEN_BRACKETS) > 0) then
-            begin
-                c_obj := TChar.Create;
-                c_obj.value := c;
-                seen_stack.push(c_obj);
-            end
-            else
-            begin
-                top := TChar(seen_stack.Pop());
-                if (pos(top.value, OPEN_BRACKETS) <> pos(c, CLOSE_BRACKETS)) then
-                begin
-                    calcIllegalScore := SCORE_MAP[pos(c, CLOSE_BRACKETS)];
-                    Break;
-                end
-            end;
-        end;
-    end;
+  Var
+    c: char;
+    c_obj: TChar;
+    top: TChar;
+    seen_stack: TObjectStack;
 
-procedure part1(lines: array of string);
-    var
-        score: integer;
-        line_content: string;
+  Begin
+    seen_stack := TObjectStack.Create;
+    calcIllegalScore := 0;
+    For c In line_content Do
+      Begin
+        If (pos(c, OPEN_BRACKETS) > 0) Then
+          Begin
+            c_obj := TChar.Create;
+            c_obj.value := c;
+            seen_stack.push(c_obj);
+          End
+        Else
+          Begin
+            top := TChar(seen_stack.Pop());
+            If (pos(top.value, OPEN_BRACKETS) <> pos(c, CLOSE_BRACKETS)) Then
+              Begin
+                calcIllegalScore := SCORE_MAP[pos(c, CLOSE_BRACKETS)];
+                Break;
+              End
+          End;
+      End;
+  End;
 
-    begin
-        score := 0;
-        for line_content in lines do
-            score := score + calcIllegalScore(line_content);
-        write('[Part1] ');
-        writeln(score);
-    end;
-
-function calcAutocompleteScore(line_content: string): Int64;
-    const
-        SCORE_MAP: array[1..4] of integer = (1, 2, 3, 4);
-
-    var
-        c: char;
-        c_obj: TChar;
-        top: TChar;
-        seen_stack: TObjectStack;
-        isValid: boolean;
-        score: Int64;
-
-    begin
-        seen_stack := TObjectStack.Create;
-        isValid := true;
-        for c in line_content do
-        begin
-            if (pos(c, OPEN_BRACKETS) > 0) then
-            begin
-                c_obj := TChar.Create;
-                c_obj.value := c;
-                seen_stack.push(c_obj);
-            end
-            else
-            begin
-                top := TChar(seen_stack.Pop());
-                if (pos(top.value, OPEN_BRACKETS) <> pos(c, CLOSE_BRACKETS)) then
-                begin
-                    isValid := false;
-                    Break;
-                end
-            end;
-        end;
-
-        score := 0;
-        if (isValid) then
-        begin
-            while seen_stack.Peek() <> Nil do
-            begin
-                top := TChar(seen_stack.Pop());
-                score := score * 5 + pos(top.value, OPEN_BRACKETS);
-            end;
-        end;
-
-        calcAutocompleteScore := score;
-    end;
-
-function findMiddle(scores: array of Int64): Int64;
-    var
-        a, b: Int64;
-        min: Int64;
-        temp: Int64;
-
-        score: Int64;
-
-    begin
-        for a := 0 to length(scores) - 2 do
-        begin
-            min := a;
-            for b := a to length(scores) - 1 do
-            begin
-                if scores[b] < scores[min] then
-                    min := b
-            end;
-            temp := scores[a];
-            scores[a] := scores[min];
-            scores[min] := temp;
-        end;
-
-        findMiddle := scores[length(scores) div 2];
-    end;
-
-procedure part2(lines: array of string);
-    var
-        index: integer;
-        score: Int64;
-        scores: array of Int64;
-        line_content: string;
-
-    begin
-        index := 0;
-        for line_content in lines do
-        begin
-            score := calcAutocompleteScore(line_content);
-            if score > 0 then
-            begin
-                SetLength(scores, index + 1);
-                scores[index] := score;
-                Inc(index);
-            end
-        end;
-
-        write('[Part2] ');
-        writeln(findMiddle(scores));
-    end;
-
-
-const
-    FILENAME = 'input.txt';
-
-var
-    fp: TextFile;
-    
-    lineno: integer;
+Procedure Part1(lines: Array Of String);
+  Var
+    score: integer;
     line_content: string;
-    lines: array of string;
 
-begin
-    AssignFile(fp, FILENAME);
-    try
-        reset(fp);
+  Begin
+    score := 0;
+    For line_content In lines Do
+      score := score + calcIllegalScore(line_content);
+    write('[Part1] ');
+    writeln(score);
+  End;
 
-        lineno := 0;
-        while not eof(fp) do
-        begin
-            readln(fp, line_content);
-            SetLength(lines, lineno + 1);
-            lines[lineno] := line_content;
-            lineno := lineno + 1;
-        end;
+Function CalcAutocompleteScore(line_content: String): Int64;
+  Const
+    SCORE_MAP: array[1..4] Of integer = (1, 2, 3, 4);
 
-        CloseFile(fp);
-    except
-        on E: EInOutError do
-            writeln('File handling error occurred. Details: ', E.Message);
-    end;
+  Var
+    c: char;
+    c_obj: TChar;
+    top: TChar;
+    seen_stack: TObjectStack;
+    isValid: boolean;
+    score: Int64;
 
-    part1(lines);
-    part2(lines);
-end.
+  Begin
+    seen_stack := TObjectStack.Create;
+    isValid := true;
+    For c In line_content Do
+      Begin
+        If (pos(c, OPEN_BRACKETS) > 0) Then
+          Begin
+            c_obj := TChar.Create;
+            c_obj.value := c;
+            seen_stack.push(c_obj);
+          End
+        Else
+          Begin
+            top := TChar(seen_stack.Pop());
+            If (pos(top.value, OPEN_BRACKETS) <> pos(c, CLOSE_BRACKETS)) Then
+              Begin
+                isValid := false;
+                Break;
+              End
+          End;
+      End;
 
+    score := 0;
+    If (isValid) Then
+      Begin
+        While seen_stack.Peek() <> Nil Do
+          Begin
+            top := TChar(seen_stack.Pop());
+            score := score * 5 + SCORE_MAP[pos(top.value, OPEN_BRACKETS)];
+          End;
+      End;
+
+    CalcAutocompleteScore := score;
+  End;
+
+Function FindMiddle(scores: Array Of Int64): Int64;
+  Var
+    a, b: Int64;
+    min: Int64;
+    temp: Int64;
+
+  Begin
+    For a := 0 To length(scores) - 2 Do
+      Begin
+        min := a;
+        For b := a To length(scores) - 1 Do
+          Begin
+            If scores[b] < scores[min] Then
+              min := b
+          End;
+        temp := scores[a];
+        scores[a] := scores[min];
+        scores[min] := temp;
+      End;
+
+    FindMiddle := scores[length(scores) Div 2];
+  End;
+
+Procedure Part2(lines: Array Of String);
+  Var
+    index: integer;
+    score: Int64;
+    scores: array Of Int64;
+    line_content: string;
+
+  Begin
+    index := 0;
+    For line_content In lines Do
+      Begin
+        score := CalcAutocompleteScore(line_content);
+        If score > 0 Then
+          Begin
+            SetLength(scores, index + 1);
+            scores[index] := score;
+            Inc(index);
+          End
+      End;
+
+    write('[Part2] ');
+    writeln(FindMiddle(scores));
+  End;
+
+
+Const
+  FILENAME = 'input.txt';
+
+Var
+  fp: TextFile;
+
+  lineno: integer;
+  line_content: string;
+  lines: array Of string;
+
+Begin
+  AssignFile(fp, FILENAME);
+  Try
+    reset(fp);
+
+    lineno := 0;
+    While Not eof(fp) Do
+      Begin
+        readln(fp, line_content);
+        SetLength(lines, lineno + 1);
+        lines[lineno] := line_content;
+        lineno := lineno + 1;
+      End;
+
+    CloseFile(fp);
+  Except
+    on E: EInOutError Do
+          writeln('File handling error occurred. Details: ', E.Message);
+End;
+
+Part1(lines);
+Part2(lines);
+End.
