167e5f4fcaa1e50f82e121b181031d630c645b0c
Tomáš Ljutenko <485503@mail.muni.cz>
2021-12-12 07:37:16 +0100

    [Day12] **

--- 12-julia/main.jl
+++ 12-julia/main.jl
@@ -46,7 +46,38 @@ function part1(edges, current, visited)
     paths
 end
 
+function part2(edges, current, visited, visited_twice)
+    paths = 0
+    for option in edges[current]
+        visited_copy = copy(visited)
+        visited_twice_copy = copy(visited_twice)
+        if option == "end"
+            paths += 1
+            continue
+        end
+
+        if option == "start"
+            continue
+        end
+
+        if islowercase(option[1])
+            if option in visited
+                if visited_twice_copy
+                    continue
+                end
+                visited_twice_copy = true
+            end
+            push!(visited_copy, option)
+        end
+        
+        paths += part2(edges, option, visited_copy, visited_twice_copy)
+    end
+    paths
+end
+
 edges = parse("input.txt")
 result = part1(edges, "start", [])
 println("[Part1] $result")
+result = part2(edges, "start", [], false)
+println("[Part2] $result")
 
