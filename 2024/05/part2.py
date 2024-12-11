71b18afdea336a6035cb29b79d42894d8217ac66
zloutek1 <zloutek1@gmail.com>
2024-12-11 18:36:16 +0100

    Day5 **

---
+++ 2024/05/part2.py
@@ -0,0 +1,26 @@
+
+with open("../input/05/input.txt", 'r') as f:
+    rules, orders = f.read().split("\n\n")
+    rules = [list(map(int, r.split("|"))) for r in rules.strip().split("\n")]
+    orders = [list(map(int, r.split(","))) for r in orders.strip().split("\n")]
+
+before = {}
+for (a, b) in rules:
+    before.setdefault(a, [])
+    before[a].append(b)
+
+mids = []
+for order in orders:
+    correct = True
+    for i in range(1, len(order)):
+        for j in range(i):
+            if order[j] in before.get(order[i], []):
+                order[i], order[j] = order[j], order[i]
+                correct = False
+
+    if not correct:
+        mid = order[len(order) // 2]
+        mids.append(mid)
+
+print(sum(mids))
+
